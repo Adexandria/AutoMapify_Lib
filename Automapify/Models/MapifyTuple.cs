66b2963a1ba777fe775fe03811564663a4d0edb7
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-12-01 10:48:30 +0100

    Map expressions to member expression

--- Automapify/Models/MapifyTuple.cs
+++ Automapify/Models/MapifyTuple.cs
@@ -2,21 +2,48 @@
 
 namespace Automapify.Models
 {
+    /// <summary>
+    /// Stores the lambda expression on how the source object will be mapped to a destination object
+    /// </summary>
+    /// <typeparam name="TSource">Source object</typeparam>
+    /// <typeparam name="TDestination">Destination object</typeparam>
     public class MapifyTuple<TSource,TDestination>
     {
+        /// <summary>
+        /// A parameterless constructor
+        /// </summary>
         public MapifyTuple()
         {
 
         }
+
+        /// <summary>
+        /// A constructor
+        /// </summary>
+        /// <param name="_sourcePredicate">Expression of the source object</param>
+        /// <param name="_destinationpredicate">Expression of the destination object</param>
         public MapifyTuple(Expression<Func<TSource,object>> _sourcePredicate, Expression<Func<TDestination, object>> _destinationpredicate)
         {
             SourcePredicate = _sourcePredicate;
-            DestinationPredicate = (MemberExpression)_destinationpredicate.Body;
+            DestinationPredicate = GetMemberExpression(_destinationpredicate);
         }
 
+        /// <summary>
+        /// Member expression of the destination object
+        /// </summary>
         public MemberExpression DestinationPredicate { get; set; }
 
+        /// <summary>
+        /// Expression of the source object
+        /// </summary>
         public Expression<Func<TSource, object>> SourcePredicate { get; set; }
 
+
+        private MemberExpression GetMemberExpression<T>(Expression<Func<T, object>> exp)
+        {
+            var member = exp.Body as MemberExpression;
+            var unary = exp.Body as UnaryExpression;
+            return member ?? (unary != null ? unary.Operand as MemberExpression : null);
+        }
     }
 }
